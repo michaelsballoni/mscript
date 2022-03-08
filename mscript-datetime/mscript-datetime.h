64cd7f259b676aca5c8c18c3f5378276392d349c
Michael Sydney Balloni <balloni.michael@yahoo.com>
2022-03-07 20:19:57 -0800

    Fleshed out the datetime business, very Windows, very file system

--- mscript-datetime/mscript-datetime.h
+++ mscript-datetime/mscript-datetime.h
@@ -2,6 +2,8 @@
 
 #include <Windows.h>
 
+#include <iomanip>
+#include <sstream>
 #include <string>
 
 namespace mscript
@@ -9,31 +11,167 @@ namespace mscript
 	class datetime
 	{
 	public:
-		datetime(const std::wstring& stringToParse, bool utc = true);
-		// https://en.cppreference.com/w/cpp/io/manip/get_time
+		// FORNOW
+		// getDatePart(str, part)
+		// getDateDiff(units, big, little)
+		// addToDate(units, amount, str)
 
-		datetime(SYSTEMTIME systemTime, bool utc = true);
-		datetime(FILETIME fileTime, bool utc = true);
-		datetime(tm* timeStruct, bool utc = true);
+		static std::string getNow(bool utc)
+		{
+			SYSTEMTIME st;
+			if (utc)
+				::GetSystemTime(&st);
+			else
+				::GetLocalTime(&st);
+			return sysTimeToString(st);
+		}
 
-		std::wstring format(const std::wstring& formatString);
-		/*
-		std::stringstream buffer;
-		buffer << std::put_time(&your_tm, "%a %b %d %H:%M:%S %Y");
-		*/
+		static std::string getFileLastModified(const std::wstring& filePath)
+		{
+			FILETIME ftCreate, ftAccess, ftWrite;
+			if (!GetFileTimes(filePath, ftCreate, ftAccess, ftWrite))
+				return "";
+			else
+				return fileTimeToString(ftWrite);
+		}
 
-		datetime getNow(bool utc = true);
+		static std::string getFileCreated(const std::wstring& filePath)
+		{
+			FILETIME ftCreate, ftAccess, ftWrite;
+			if (!GetFileTimes(filePath, ftCreate, ftAccess, ftWrite))
+				return "";
+			else
+				return fileTimeToString(ftCreate);
+		}
 
-		datetime getFileLastModified(const std::wstring& filePath);
-		datetime getFileCreated(const std::wstring& filePath);
+		static std::string getFileLastAccessed(const std::wstring& filePath)
+		{
+			FILETIME ftCreate, ftAccess, ftWrite;
+			if (!GetFileTimes(filePath, ftCreate, ftAccess, ftWrite))
+				return "";
+			else
+				return fileTimeToString(ftAccess);
+		}
 
-		void toUtc();
-		void toLocal();
+		static std::string toUtc(const std::string& str)
+		{
+			SYSTEMTIME st = sysTimeFromString(str);
+			FILETIME ft{};
+			if (!::SystemTimeToFileTime(&st, &ft))
+				return "";
 
-		void touch(const std::wstring& filePath);
+			FILETIME localFt{};
+			if (!::LocalFileTimeToFileTime(&ft, &localFt))
+				return "";
 
-	private:
-		bool m_isUtc;
-		SYSTEMTIME m_sysTime;
+			if (!::FileTimeToSystemTime(&localFt, &st))
+				return "";
+
+			return sysTimeToString(st);
+		}
+
+		static std::string toLocal(const std::string& str)
+		{
+			SYSTEMTIME st = sysTimeFromString(str);
+			FILETIME ft{};
+			if (!::SystemTimeToFileTime(&st, &ft))
+				return "";
+
+			FILETIME localFt{};
+			if (!::FileTimeToLocalFileTime(&ft, &localFt))
+				return "";
+
+			if (!::FileTimeToSystemTime(&localFt, &st))
+				return "";
+
+			return sysTimeToString(st);
+		}
+
+		BOOL touch(const std::wstring& filePath)
+		{
+			SYSTEMTIME st;
+			::GetSystemTime(&st);
+			FILETIME ft{};
+			if (!::SystemTimeToFileTime(&st, &ft))
+				return FALSE;
+
+			HANDLE hFile =
+				::CreateFile
+				(
+					filePath.c_str(),
+					GENERIC_WRITE,
+					FILE_SHARE_READ,
+					nullptr,
+					OPEN_EXISTING,
+					0,
+					nullptr
+				);
+			if (hFile == nullptr)
+				return FALSE;
+
+			BOOL success = ::SetFileTime(hFile, nullptr, nullptr, &ft);
+			::CloseHandle(hFile);
+			return success;
+		}
+
+		static BOOL GetFileTimes(const std::wstring& filePath, FILETIME& ftCreate, FILETIME& ftAccess, FILETIME& ftWrite)
+		{
+			HANDLE hFile =
+				::CreateFile
+				(
+					filePath.c_str(),
+					GENERIC_READ,
+					FILE_SHARE_READ,
+					nullptr,
+					OPEN_EXISTING,
+					0,
+					nullptr
+				);
+			if (hFile == nullptr)
+				return FALSE;
+
+			BOOL success = ::GetFileTime(hFile, &ftCreate, &ftAccess, &ftWrite);
+			::CloseHandle(hFile);
+			return success;
+		}
+
+		static std::string fileTimeToString(const FILETIME& ft)
+		{
+			SYSTEMTIME st{};
+			if (!FileTimeToSystemTime(&ft, &st))
+				return "";
+			else
+				return sysTimeToString(st);
+		}
+
+		static std::string sysTimeToString(const SYSTEMTIME& sysTime)
+		{
+			std::stringstream msg;
+			msg << sysTime.wYear << "-"
+				<< std::setw(2) << std::setfill('0') << sysTime.wMonth << "-"
+				<< std::setw(2) << std::setfill('0') << sysTime.wDay << " "
+				<< std::setw(2) << std::setfill('0') << sysTime.wHour << ":"
+				<< std::setw(2) << std::setfill('0') << sysTime.wMinute << ":"
+				<< std::setw(2) << std::setfill('0') << sysTime.wSecond << "."
+				<< std::setw(3) << std::setfill('0') << sysTime.wMilliseconds;
+			return msg.str();
+		}
+
+		static SYSTEMTIME sysTimeFromString(const std::string& stringToParse)
+		{
+			SYSTEMTIME st{};
+			sscanf_s
+			(
+				stringToParse.c_str(), 
+				"%d-%d-%d %d:%d:%d.%d",
+				&st.wYear,
+				&st.wMonth,
+				&st.wDay,
+				&st.wHour,
+				&st.wMinute,
+				&st.wMilliseconds
+			);
+			return st;
+		}
 	};
 }
