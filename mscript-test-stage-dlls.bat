058f0a6160d3079d9ab34486dda3d8328e77a1ea
Michael Sydney Balloni <balloni.michael@yahoo.com>
2022-03-10 18:03:39 -0800

    Updated batch for stagings DLLs for test runner

--- mscript-test-stage-dlls.bat
+++ mscript-test-stage-dlls.bat
@@ -1,3 +1,3 @@
-xcopy /Y x64\Release\mscript-dll-sample.dll mscript-test-scripts\.
-xcopy /Y x64\Release\mscript-timestamp.dll mscript-test-scripts\.
+xcopy /Y Win32\Release\mscript-dll-sample.dll mscript-test-scripts\.
+xcopy /Y Win32\Release\mscript-timestamp.dll mscript-test-scripts\.
 pause
